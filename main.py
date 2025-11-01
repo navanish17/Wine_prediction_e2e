44fdede581ad08043f51d1d8b3305ec6cc5a4656
navanish17 <navnishpande17@gmail.com>
2025-11-01 19:47:59 +0530

    Model_evaluation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Wine_prediction_e2e.pipeline.Data_ingestion_stage import DataIngestionTrain
 from Wine_prediction_e2e.pipeline.Data_validation_stage import DataValidationTrainingPipeline
 from Wine_prediction_e2e.pipeline.Data_transformation_stage import DataTransformationTrainingPipeline
 from Wine_prediction_e2e.pipeline.Model_trainer_stage import ModelTrainerTrainingPipeline
-
+from Wine_prediction_e2e.pipeline.Model_evaluation_stage import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "<<<<<<<Data Ingestion stage>>>>>>>>>"
 
@@ -44,6 +44,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
